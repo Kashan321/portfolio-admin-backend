0d659ede0f6fda0db5d21c574dbff3579cde403b
kashan haider <kashanhaider3241@gmail.com>
2025-03-15 12:34:08 +0500

    debugging

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDb = () => {
     const Mongo_URI = process.env.MONGO_URI
     try {
-        const db = mongoose.connect(Mongo_URI, {
+        const db = mongoose.connect("mongodb+srv://kashan3241:kashan@cluster0.x5am9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
@@ -13,3 +13,5 @@ export const connectDb = () => {
     }
 }
 
+connectDb()
+export default connectDb
